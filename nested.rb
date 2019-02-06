19f828c3191e783a55192e266e4fb6abd0bf015c
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-06 08:05:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,8 +83,6 @@ def adding_matz
   programmer_hash[:yukihiro_matsumoto] = {}
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
   programmer_hash[:yukihiro_matsumoto][:languages] = []
-  programmer_hash[:yukihiro_matsumoto][:languages].push("LISP")
-  programmer_hash[:yukihiro_matsumoto][:languages].push("C")
     
 end
 
