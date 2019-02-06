e8042a923fa070a32d846705daba0e30253fbaff
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-06 07:42:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,11 +15,13 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  hopper[:known_for]
 
 end
 
 def alan_kay_is_known_for
+	
+	
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
